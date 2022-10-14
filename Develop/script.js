cf73709db5b8ec97ee6e0d682c03574d698259ba
Genevieve Ferrari <genferrari@gmail.com>
2022-10-13 18:06:04 -0700

    added psuedo code and practice loops

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+/* password requirements: at least 8 characters & no more that 128
+special characters (include or no lowercase, upper case, numeric and/or special characters) 
+*/
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,4 +18,58 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+  /* if (How many characters do you want you password to be) 
+      else if
+      else (you fucked up start again)
+    */
+
+    var frodo = prompt("WHATS UP");
+    console.log(frodo)
+
+    if (frodo <= 7) {
+      alert("Please choose a number between 8-128")
+    }
+    else if (frodo >= 128) {
+      alert ("Please choose a number between 8-128")
+    }
+    
+  
+
+    var isMichi = confirm ("Is this michi")
+
+    var auggie = confirm("Lowercase?")
+    console.log (michi,auggie)
+    //if password is less than or equal 7 then function ends
+    // if password is between 8-128 then ask if you want upper case characters CONFIRM (BOOLEAN), then CONFIRM lower case, CONFIRM numeric, CONFIRM specials 
+    //boolean OR *mdn boolean operators 
 }
+
+/* will probably need to use "prompt" 
+when I press genterate password, prompt box asks for number of characters 
+5 prompts: lenghth of password (PROMPT), lowercase, uppercase, numeric, & special characters (CONFIRM) VARIABLES 
+
+For loop potentially for lower case upper case 
+
+If not right characters - alert box (ok only)
+
+alert - just shows the message - ok 
+prompt - input box with ok and cancel 
+confirm - else (message with an ok & cancel)
+
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
